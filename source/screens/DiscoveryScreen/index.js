baf58c2f32977af2d70fd431f07fd68094548ee1
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2023-10-18 14:37:24 +0700

    discovery init

--- source/screens/DiscoveryScreen/index.js
+++ source/screens/DiscoveryScreen/index.js
@@ -1,10 +1,104 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import PostsProfile from "../../components/TabProfile/Posts";
+const DiscoveryScreen = () => {
+  return (
+    <View style={[styles.container, { backgroundColor: "white" }]}>
+      <View style={styles.horizontalContainer}>
+        {/* //     <AntDesign
+    //       style={{ marginLeft: "30px" }}
+    //       name="search1"
+    //       size={20}
+    //       color="black"
+    //     /> */}
 
-const DiscoveryScreen = () => (
-  <View style={{backgroundColor: "blue", height: 1500}}>
-    <Text style={{textAlign: 'center', marginTop: 300, fontSize: 30, color: 'white'}}>Discovery</Text>
-  </View>
-)
+        <TextInput style={styles.TextInput} placeholder="Tìm kiếm"></TextInput>
+      </View>
+      <PostsProfile />
+    </View>
+  );
+};
 
 export default DiscoveryScreen;
+const styles = StyleSheet.create({
+  verticalContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginLeft: 20,
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  container: {
+    flex: 1,
+    // flexDirection: "column",
+    backgroundColor: "#fff",
+    alignContent: "center",
+
+    // justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  TextInput: {
+    width: "90%",
+    // alignContent: "center",
+    // borderColor: "gray",
+    borderRadius: 5,
+    marginBottom: 20,
+    backgroundColor: "#eee",
+    height: 36,
+  },
+  horizontalContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 10,
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    marginRight: 30,
+    marginLeft: 10,
+  },
+  numberText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  infoText: {
+    fontSize: 16,
+    color: "gray",
+    width: 90,
+    // alignContent: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    // textAlign: "",
+    marginRight: 10,
+    // width: "60%",
+  },
+  nameText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginLeft: 10,
+  },
+
+  btnProfile: {
+    backgroundColor: "#eee",
+    borderRadius: 5,
+    // borderWidth: 1,
+    // borderColor: "#ddd",
+    padding: 5,
+    // width: "40%",
+    width: 150,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 30,
+    marginLeft: 10,
+    // width: "12em",
+  },
+
+  textBtn: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#000",
+  },
+});
